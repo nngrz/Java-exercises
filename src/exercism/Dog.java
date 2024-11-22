7852f6a0f2a20672b3f79a6454823f7913a194ed
Ningrui Zhang <ningrui1227@gmail.com>
2024-11-22 15:18:39 +0000

    fix the setter method

--- src/exercism/Dog.java
+++ src/exercism/Dog.java
@@ -2,38 +2,34 @@ class Dog {
     String name;
     String breed;
 
-    // get the name and breed
+    // constructor for the Dog class that initializes the name and breed variables
     public Dog(String name, String breed) {
         this.name = name;
         this.breed = breed;
     }
 
-    // reset the name and breed
-    public void setNameBreed(String setName, String setBreed) {
-        name = setName;
-        breed = setBreed;
+    // method to retrieve the name of the dog
+    public String getName() {
+        return name;
     }
 
-    // print the name and breed
-    void print() {
-        System.out.println(name + " is a " + breed);
+    // method to retrieve the breed of the dog
+    public String getBreed() {
+        return breed;
     }
 
-    // print the resetted name and breed
-    void setPrint() {
-        System.out.println(name + " is a " + breed);
+    // method to set name and breed of the dog
+    void setNameBreed(String name, String breed) {
+        this.name = name;
+        this.breed = breed;
     }
 
     public static void main(String[] args) {
-        // get the name and breed, and print
         Dog dog1 = new Dog("Albert", "Golden Retriever");
-        dog1.print();
         Dog dog2 = new Dog("Bun Bun", "German Shepherd");
-        dog2.print();
-        // reset the name and breed, and print
-        Dog dog1Set = new Dog ("Cindy", "Sausage Dog");
-        dog1Set.setPrint();
-        Dog dog2Set = new Dog ("Yudian", "border Collie");
-        dog2Set.setPrint();
+        dog1.setNameBreed("Cindy", "Sausage Dog");
+        System.out.println(dog1.getName() + " is a " + dog1.getBreed());
+        dog2.setNameBreed("Yudian", "border Collie");
+        System.out.println(dog2.getName() + " is a " + dog2.getBreed());
     }
 }
