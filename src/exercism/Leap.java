d42251adfa0558bd722378ae5b18192ee599dbb1
Ningrui Zhang <ningrui1227@gmail.com>
2024-11-26 21:59:58 +0000

    adjust format

--- src/exercism/Leap.java
+++ src/exercism/Leap.java
@@ -1,6 +1,6 @@
 class Leap {
     boolean isLeapYear(int year) {
-       return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+       return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
     }
 
     public static void main(String[] args) {
