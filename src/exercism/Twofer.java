9273483c1964244e1badd3df892e69bd6ce3b081
Ningrui Zhang <ningrui1227@gmail.com>
2024-11-27 11:34:41 +0000

    add Twofer.java

---
+++ src/exercism/Twofer.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Twofer {
+    public String twofer(String name) {
+       if (name == null || name.isEmpty()) {
+           name = "you";
+       }
+       return "One for " + name + ", one for me.";
+    }
+    
+    public static void main(String[] args) {
+        Twofer twofer = new Twofer();
+        Scanner s = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name = s.nextLine();
+        s.close();
+        System.out.println(twofer.twofer(name));
+    }
+}
