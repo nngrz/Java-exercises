209a7366651425a1b84f7f85547c9371108d258f
Ningrui Zhang <ningrui1227@gmail.com>
2024-12-05 14:54:13 +0000

    adjust Darts.java

--- src/exercism/Darts.java
+++ src/exercism/Darts.java
@@ -1,11 +1,11 @@
 class Darts {
     static int score(double xOfDart, double yOfDart) {
-        double userRadius = Math.pow(xOfDart, 2) + Math.pow(yOfDart, 2);
-        if (userRadius >= 0 && userRadius <= Math.pow(1.0, 2)) {
+        double userRadius = xOfDart * xOfDart + yOfDart * yOfDart;
+        if (userRadius <= 1.0 * 1.0) {
             return 10;
-        } else if (userRadius > Math.pow(1.0, 2) && userRadius <= Math.pow(5.0, 2)) {
+        } else if (userRadius <= 5.0 * 5.0) {
             return 5;
-        } else if (userRadius > Math.pow(5.0, 2) && userRadius <= Math.pow(10.0, 2)) {
+        } else if (userRadius <= 10.0 * 10.0) {
             return 1;
         } else {
             return 0;
