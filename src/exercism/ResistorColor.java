c911d4a8bd9dc7c84736d0a7c3473e6a4d071614
Ningrui Zhang <ningrui1227@gmail.com>
2024-12-08 23:34:47 +0000

    add file ResistorColor.java

---
+++ src/exercism/ResistorColor.java
@@ -0,0 +1,26 @@
+class ResistorColor {
+    int code = 0;      
+    String[] colorNames = {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
+
+    int colorCode(String color) {
+
+        for (int i = 0; i < colorNames.length; i++) {
+            if (colorNames[i].equals(color)) {
+                code = i;
+            }
+        }
+        return code;
+    }
+
+    String[] colors() {
+        return colorNames;
+    }
+
+    public static void main (String[] args) {
+        ResistorColor rColor = new ResistorColor();
+        System.out.println(rColor.colorCode("red"));
+        for (String color : rColor.colors()){
+            System.out.println(color);
+        }
+    }
+}
