649f9048ff4d14cbc4a3955de1764cb0a9269803
Ningrui Zhang <ningrui1227@gmail.com>
2024-12-09 11:15:21 +0000

    delete variable int code

--- src/exercism/ResistorColor.java
+++ src/exercism/ResistorColor.java
@@ -2,11 +2,9 @@ class ResistorColor {
     String[] colorNames = {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
 
     int colorCode(String color) {
-        int code = -1;
         for (int i = 0; i < colorNames.length; i++) {
             if (colorNames[i].equals(color)) {
-                code = i;
-                return code;
+                return i;
             }
         }
         throw new IllegalArgumentException("Enter a valid color.");
@@ -18,7 +16,7 @@ class ResistorColor {
 
     public static void main (String[] args) {
         ResistorColor rColor = new ResistorColor();
-        System.out.println(rColor.colorCode("eee"));
+        System.out.println(rColor.colorCode("red"));
         for (String color : rColor.colors()){
             System.out.println(color);
         }
