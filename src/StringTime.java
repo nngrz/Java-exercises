de3eec695105a2aaecd73a31057187b143f7c996
Ningrui Zhang <ningrui1227@gmail.com>
2024-12-16 13:45:39 +0000

    add StringTime.java

---
+++ src/StringTime.java
@@ -0,0 +1,17 @@
+class StringTime {
+
+    String printer(int length) {
+        String result = "";
+        for (int i = 0; i < length; i++) {
+            result = result + "D";
+        }
+        return "Finished creating a string consisting of " + length + " characters.";
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Enter length: " + args[0]);
+
+        StringTime stringTime = new StringTime();
+        System.out.println(stringTime.printer(Integer.parseInt(args[0])));
+    }
+}
