56f25e5b0233f3444095dc179f346a6858207a10
Ningrui Zhang <ningrui1227@gmail.com>
2024-12-16 14:57:23 +0000

    remove method buildString

--- src/StringBuilderTime.java
+++ src/StringBuilderTime.java
@@ -1,17 +1,14 @@
 class StringBuilderTime {
 
-    String buildString(int length) {
+    public static void main(String[] args) {
+        System.out.println("Enter length: " + args[0]);
+        int length = Integer.parseInt(args[0]);
+
         StringBuilder result = new StringBuilder();
         for (int i = 0; i < length; i++) {
             result.append("D");
         }
-        return "Finished creating a string consisting of " + length + " characters.";
-    }
 
-    public static void main(String[] args) {
-        System.out.println("Enter length: " + args[0]);
-
-        StringBuilderTime stringBuilderTime = new StringBuilderTime();
-        System.out.println(stringBuilderTime.buildString(Integer.parseInt(args[0])));
+        System.out.println("Finished creating a string consisting of " + length + " characters.");
     }
 }
--- src/StringTime.java
+++ src/StringTime.java
@@ -1,17 +1,14 @@
 class StringTime {
 
-    String buildString(int length) {
+    public static void main(String[] args) {
+        System.out.println("Enter length: " + args[0]);
+        int length = Integer.parseInt(args[0]);
         String result = "";
+
         for (int i = 0; i < length; i++) {
             result = result + "D";
         }
-        return "Finished creating a string consisting of " + length + " characters.";
-    }
 
-    public static void main(String[] args) {
-        System.out.println("Enter length: " + args[0]);
-
-        StringTime stringTime = new StringTime();
-        System.out.println(stringTime.buildString(Integer.parseInt(args[0])));
+        System.out.println("Finished creating a string consisting of " + length + " characters.");
     }
 }
