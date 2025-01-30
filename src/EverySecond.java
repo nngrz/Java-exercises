90f334c7cfb02ce6e0d7a6d0d9943114cc3f4760
Ningrui Zhang <ningrui1227@gmail.com>
2025-01-30 21:38:54 +0800

    adjust variable name from string to result; optimize error handling

--- src/EverySecond.java
+++ src/EverySecond.java
@@ -1,20 +1,24 @@
 public class EverySecond {
     public static String everysec(String s) {
         if (s.isEmpty()) {
-            throw new IllegalArgumentException("You must enter at least 1 character");
+            throw new IllegalArgumentException("You must enter at least 1 character.");
         } 
         
-        StringBuilder string = new StringBuilder();
+        StringBuilder result = new StringBuilder();
 
         for (int i = 0; i < s.length(); i += 2) {          
-            string.append(s.charAt(i));
+            result.append(s.charAt(i));
         }
-        return string.toString();
+        return result.toString();
     }
 
     public static void main(String[] args) {
-        System.out.println(everysec("ABCDE"));
-        System.out.println(everysec("123456")); // Expected output: "135"
-        System.out.println(everysec("")); // Will throw an exception
+        try {
+            System.out.println(everysec("ABCDE")); // Expected output: "ACE";
+            System.out.println(everysec("123456")); // Expected output: "135";
+            System.out.println(everysec("")); // Will throw an exception;
+        } catch (IllegalArgumentException e) {
+            System.err.println("Error: " + e.getMessage());
+        }
     }
 }
