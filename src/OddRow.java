8385f988c4acf1b38c65da1cd78966d3f13a5e90
Ningrui Zhang <ningrui1227@gmail.com>
2025-02-13 15:54:30 +0000

    simplify oddRow
    adjust variable int count.
    remove variable int newRowIndex.

---
+++ src/OddRow.java
@@ -0,0 +1,31 @@
+public class OddRow {
+    
+    public static int[][] oddRow(int[][] a) {
+        // Count the number of rows with odd indices
+        int count = (a.length + 1) / 2;
+
+        // Create a new array to store rows with odd indices
+        int[][] newArr = new int[count][a[0].length];
+        for (int i = 0; i < a.length; i++) {
+            if (i % 2 != 0) {
+               newArr[i/2] = a[i];
+            }
+        }
+        return newArr;
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = { {1, 2, 3},
+                        {4, 5, 6},
+                        {7, 8, 9},
+                        {10, 11, 12}};
+        
+        int[][] result = oddRow(arr);
+        for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < result[i].length; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
